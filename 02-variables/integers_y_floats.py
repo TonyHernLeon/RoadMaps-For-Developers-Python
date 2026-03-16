1a66c73e00f180317aed8eea9a8c67b6c8869641
Antonio Hernández León <tonyhernleon@Starkiller-M4.local>
2026-03-16 16:37:29 +0100

    Tipos de variables

---
+++ 02-variables/integers_y_floats.py
@@ -0,0 +1,10 @@
+numeroPropio = 1
+
+print(numeroPropio)
+print("Tipo de variable: ", type(numeroPropio))
+
+numeroPropio = 5.3
+print(numeroPropio)
+print("Tipo de variable: ", type(numeroPropio))
+
+## Con el comando type(nomVariable) te da el tipo de la variable
