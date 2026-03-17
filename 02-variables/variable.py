01ccfeec21f13e2d0b0c6d9361e04c1bf988f1fd
Antonio Hernandez <antoniohl91@gmail.com>
2026-03-17 14:04:12 +0100

    Arreglos de pylint

--- 02-variables/variable.py
+++ 02-variables/variable.py
@@ -1,11 +1,16 @@
 # Ejemplos de variables
-nombre = "Antonio";
-segundoNombre = 'Juan'
+nombre = "Antonio"
+segundoNombre = "Juan"
 numeroUno = 555
 nombreParaConcatenar = "Jose"
 nombreParaInput = input("¿Que nombre le pega a Jose? ")
 
-print('El primer nombre es: '+ nombre)
-print("El segundo nombre es: "+segundoNombre)
+print("El primer nombre es: " + nombre)
+print("El segundo nombre es: " + segundoNombre)
 print("El numero de la variable es:", numeroUno)
-print("EL nombre que te gusta para concatenarlo con {nombreParaConcatenar} es: "+ nombreParaConcatenar + " " + nombreParaInput)
+print(
+    "EL nombre que te gusta para concatenarlo con {nombreParaConcatenar} es: "
+    + nombreParaConcatenar
+    + " "
+    + nombreParaInput
+)
