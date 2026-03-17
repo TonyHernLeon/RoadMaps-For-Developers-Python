763a1f678c80cc15cbf4c0abb33189fca6214d84
Antonio Hernandez <antoniohl91@gmail.com>
2026-03-17 16:00:54 +0100

    Reestructuración

---
+++ nivel básico/01-print_input/Cerveceria/cerveceria.py
@@ -0,0 +1,4 @@
+print("El nombre de tu cerveceria\nes " +
+      + input("¿Qué ciudad te gustaría visitar? "+ " "+ 
+              +input("¿Cúal es tu color favorito?")+
+              "\nFelicitaciones!!"))
