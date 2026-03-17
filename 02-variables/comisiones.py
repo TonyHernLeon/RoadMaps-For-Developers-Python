064e161021a9d0d535d9ec309da09ab3038c6c37
Antonio Hernandez <antoniohl91@gmail.com>
2026-03-17 14:16:22 +0100

    Añado comentario de aclaración del ejercicio

--- 02-variables/comisiones.py
+++ 02-variables/comisiones.py
@@ -1,4 +1,10 @@
-"""Programa de ingresos"""
+"""
+Programa de ingresos:
+Este programa consiste en simular un programa de calculo de ingresos, con comisiones
+por venta. El usuario debe ingresas, nombre y número de ventas, y el programa
+debe tomar esos datos y imprimir por pantalla los ingresos de su ventas, con
+una comisión del 13%.
+"""
 
 NOMBRE_USER = input("¿Cuál es tu nombre?: ")
 VENTAS_USER = input("¿Cuánto has vendido?: ")
