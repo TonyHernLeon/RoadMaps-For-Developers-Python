973f18f8badede490dafea9af691de5becfd511a
Antonio Hernández <64372973+TonyHernLeon@users.noreply.github.com>
2026-03-17 16:06:20 +0100

    Delete nivel básico directory

--- nivel básico/01-print_input/Cerveceria/cerveceria.py
+++
@@ -1,4 +0,0 @@
-print("El nombre de tu cerveceria\nes " +
-      + input("¿Qué ciudad te gustaría visitar? "+ " "+ 
-              +input("¿Cúal es tu color favorito?")+
-              "\nFelicitaciones!!"))
