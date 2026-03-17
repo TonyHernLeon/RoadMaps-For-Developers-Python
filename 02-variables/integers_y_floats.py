02d4881184cbe82c049142a829343666cf09a515
Antonio Hernández León <tonyhernleon@Starkiller-M4.local>
2026-03-17 11:20:36 +0100

    Formas de formatear cadenas

--- 02-variables/integers_y_floats.py
+++ 02-variables/integers_y_floats.py
@@ -7,4 +7,4 @@ numeroPropio = 5.3
 print(numeroPropio)
 print("Tipo de variable: ", type(numeroPropio))
 
-## Con el comando type(nomVariable) te da el tipo de la variable
+# Con el comando type(nomVariable) te da el tipo de la variable
