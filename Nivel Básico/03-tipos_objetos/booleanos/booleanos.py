0c38f5f89e22e7bd9939dba15277ff703283edbb
Antonio Hernandez <antoniohl91@gmail.com>
2026-03-18 16:31:55 +0100

    Tipo de objetos Boolean

---
+++ Nivel Básico/03-tipos_objetos/booleanos/booleanos.py
@@ -0,0 +1,15 @@
+"""
+Módulo de Booleanos
+"""
+
+# Varibles
+num_booleano = bool(5 > 6)
+booleano_true = True
+booleano_fale = False
+
+print(type(num_booleano))
+print(num_booleano)
+
+print(bool(25 * 0.5 == 5))
+
+print(25**0.5)
