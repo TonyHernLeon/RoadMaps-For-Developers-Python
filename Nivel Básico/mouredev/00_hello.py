a1073d8fec304283f30a66e65952d4d3ee15a282
Antonio Hernandez <antoniohl91@gmail.com>
2026-03-19 16:37:56 +0100

    Primer día de curso: Hola Mundo y variables en Python

---
+++ Nivel Básico/mouredev/00_hello.py
@@ -0,0 +1,23 @@
+# Esto es un comentario: Hola Mundo
+# Nuestro hola mundo en Python
+print("Hola Python")
+print("Hola Python")
+
+
+"""
+Este es un comentario 
+de varias lineas 
+"""
+
+"""Este también es un comentario 
+de varias lineas 
+"""
+
+
+# Consultar el tipo de dato
+print(type("Hola Python"))  # Tipo 'str'
+print(type(5))  # Tipo 'int'
+print(type(0.5))  # Tipo 'float'
+print(type(True))  # Tipo 'bool'
+
+print(type(print("Mi cadena de Texto")))
---
+++ Nivel Básico/mouredev/01_variables.py
@@ -0,0 +1,34 @@
+# Variables
+"""Aunque Python es muy flexible a la hora de
+crear variables. Es verdad, que las formas válidas son:
+· Snake Case => snake_case.
+· CamelCase => camelCase
+· excepsiones => _if. Solo para usar en con palabras reservadas. Poco usadas."""
+
+## Variables
+my_string_variable = "Mi Variable String"
+print(my_string_variable)
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = True
+print(my_bool_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable, type(my_int_to_str_variable))
+
+## Variables en una sola linea. || Cuidado con abusar con esta sintaxis!!
+name, surname, alias, age = "Antonio", "Hernandez", "Tony_HernLeon", 34
+print("Soy", surname, name, "\nMi alias es:", alias, "\nMi edad es:", age, "\n")
+
+## Concatenación de variables en un print
+print(
+    my_string_variable,
+    my_int_variable,
+    my_int_to_str_variable,
+    my_bool_variable,
+)
+print("Este es el valor de:", my_bool_variable)
+
+## Algunas funciones de Sistema
+print(len(my_string_variable))
